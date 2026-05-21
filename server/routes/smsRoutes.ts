8745a6cdab319bd48f7cba59ed49b012e9555326
earmahsakyi <sakyi5375@gmail.com>
2026-05-21 10:44:20 +0000

    removed extra space on phone

--- server/routes/smsRoutes.ts
+++ server/routes/smsRoutes.ts
@@ -5,7 +5,7 @@ import { validateBody } from "../middleware/validate.js";
 
 const router = Router();
 
-router.post('/send',validateBody(['order_id',' phone','message','trigger_type']), protect, sendSms);
+router.post('/send',validateBody(['order_id','phone','message','trigger_type']), protect, sendSms);
 router.get('/', protect, getSmsLogs);
 
 export default router;
